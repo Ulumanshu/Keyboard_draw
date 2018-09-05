c447f8bd8e3491ea646e1d4c4b51e7508e8451b9
Ulumanshu <karolis_kesminas@yahoo.com>
2018-09-05 04:47:44 +0300

    fileshephard working, maybe bugs, because messy

--- model/Train_Former.py
+++ model/Train_Former.py
@@ -91,7 +91,6 @@ class Train_Former:
         sjson = self.Json_Self
         comp_dict_s = {}
         comp_dict_t = {}
-
         for key_s, value_s in sjson["Save_dir"].items():
             comp_dict_s[key_s] = value_s.get("Min_fc")
         for key_t, value_t in sjson["Train_dir"].items():
@@ -144,47 +143,21 @@ class Train_Former:
             dir_name_abr_root = re.search(r"([^/]+$)", str(e)).group(1)
             dir_cnt, dir_ls = Train_Former.count_dir(e)
             target_dir = self.train_dir + "/" + "Classifajar" + "/" + dir_name_abr_root
-            print(target_dir)
             count_target, file_list = Train_Former.count_file(target_dir)
-            print(file_list)
-            print(range(count_target, clasifaj_count))
             for c, i in zip(range(count_target, clasifaj_count), dir_ls):
                 save_dir = e + "/" + i
-                print("save_dir: " + save_dir)
                 i_fcount, file_list_i = Train_Former.count_file(save_dir)
-                #itera = 0
                 for file in file_list_i:
-                    print(file_list_i)
                     file_nr = re.search(r"([^_]+$)", str(file)).group(1)
                     file_nr = re.search(r"(.)", str(file_nr)).group(1)
-                    print("file nr: " + file_nr)
-                    print("nook: " + str(int((c // dir_cnt) + 1)))
                     nook = str(int(c // dir_cnt))
-                    print(type(nook))
-                    if file_nr == nook:
-                        #while itera < 1:
+                    if eval(file_nr) == eval(nook) + 1:
                         copied_file = Train_Former.read_file(save_dir, file)
                         Train_Former.save_file(target_dir, file, copied_file)
                         break
 
-                            # itera += 1
-                            # print(itera)
-
-
-
-
         return print(comp_dict_cl)
 
-        # s_lower = sjson["Save_dir"]["lowercase"]["Min_fc"]
-        # s_upper = sjson["Save_dir"]["uppercase"]["Min_fc"]
-        # s_numbr = sjson["Save_dir"]["numbers"]["Min_fc"]
-        # t_lower = sjson["Train_dir"]["lowercase"]["Min_fc"]
-        # t_upper = sjson["Train_dir"]["uppercase"]["Min_fc"]
-        # t_numbr = sjson["Train_dir"]["numbers"]["Min_fc"]
-        #
-        #
-        # if
-        #return print("Yay")
 
     @staticmethod
     def read_file(dir_c, fname):
